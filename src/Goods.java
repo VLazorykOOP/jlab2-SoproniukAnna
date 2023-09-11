5332a421cdffb72a9301db77e93a504d623343e5
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2023-09-11 20:38:29 +0300

    Task2/Final

--- src/Goods.java
+++ src/Goods.java
@@ -1,20 +1,79 @@
-import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.Scanner;
 public class Goods
 {
     private String name;
     private Date date;
     private double price;
     private int count;
-    private int invoice;
+    private String invoice;
 
-    public double changePrice()
+    public Goods(String name, Date date, double price, int count, String invoice)
+    {
+        this.name = name;
+        this.date = date;
+        this.price = price;
+        this.count = count;
+        this.invoice = invoice;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Date getDate()
+    {
+        return date;
+    }
+
+    public double getPrice()
     {
         return price;
     }
-    public int changeCount(){return count;}
-    public double calculationPriceProduct(){return 2;}
-    public String toString() {return name;}
-    public boolean equals(Object obj){return true;}
+    public double setPrice(double newPrice)
+    {
+        this.price = newPrice;
+        return this.price;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+    public int setCount(int newCount)
+    {
+        this.count = newCount;
+        return this.count;
+    }
+
+    public String getInvoice()
+    {
+        return invoice;
+    }
+
+
+    public double calculationPriceProduct()
+    {
+        return this.price * this.count;
+    }
+    @Override
+    public String toString()
+    {
+        String s;
+        s = getName() + "\t" + getDate() + "\t" + getPrice() + "\t" + getCount() + "\t" + getInvoice();
+        return s;
+    }
+    @Override
+    public boolean equals(Object obj)
+    {
+        if ( obj instanceof Goods )
+        {
+            Goods obj1 = (Goods) obj;
+            if ( name == obj1.getName() && date == obj1.getDate() && price == obj1.getPrice() && count == obj1.getCount() && invoice == obj1.getInvoice())
+                return true;
+        }
+        return false;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
-public class Main {
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main
+{
     public static void main(String[] args)
     {
-        System.out.println("Java Lab 2 ");
+        Scanner in = new Scanner(System.in);
+
+        Goods milk = new Goods("Milk", new Date(), 30.05, 5, "135hr4");
+        Goods bread = new Goods("Bread", new Date(), 20, 10, "73hfts");
+
+        System.out.println("\t \t \t Java Lab 2 ");
+        System.out.println("\t \t \t Task 1");
+
+        System.out.println("\t \t \t Info about " + milk.getName());
+        System.out.println(milk.toString());
+        System.out.println();
+        System.out.println("\t \t \t Info about " + bread.getName());
+        System.out.println(bread.toString());
+
+        System.out.println();
+        System.out.print("Input new count for " + milk.getName() + ": ");
+        int count = in.nextInt();
+        milk.setCount(count);
+
+        System.out.println();
+        System.out.print("Input new price for " + milk.getName() + ": ");
+        double price = in.nextDouble();
+        milk.setPrice(price);
+
+        System.out.println();
+        System.out.println("\t \t \t New info about " + milk.getName());
+        System.out.println(milk.toString());
+
+        System.out.println();
+        System.out.println("\t \t \t Price of products ");
+        System.out.println(milk.calculationPriceProduct());
+
+        System.out.println();
+        System.out.println("\t \t \t Comparison of the " + milk.getName() + " and the " + milk.getName());
+        System.out.println(milk.equals(milk));
+        System.out.println("\t \t \t Comparison of the " + milk.getName() + " and the " + bread.getName());
+        System.out.println(milk.equals(bread));
+
+
     }
 }
